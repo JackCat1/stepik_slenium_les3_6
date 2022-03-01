e04f86e1ca7d0fbf3bd55298c3e67faecbc00677
Максим Котов <kotov.maksim1989@gmail.com>
2022-03-01 09:25:28 +0600

    Assert add

--- test_items.py
+++ test_items.py
@@ -1,9 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
+def found_element_by_selector(browser, selector):
+    try:
+        browser.find_element(By.CSS_SELECTOR, selector)
+    except NoSuchElementException:
+        return False
+    return True
+
+
 def test_guest_should_see_basket_add_btn(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form button[type='submit']")
+    assert found_element_by_selector(browser, "#add_to_basket_form button[type='submit']"), 'Basket submit ' \
+                                                                                            'button not found'
